9133c3965741fbac1e91480ba7625409af6a5f1d
Juan Mangueira <juanmangueiraa@gmail.com>
2024-05-08 20:48:29 -0300

    script ordenador

---
+++ ordenador.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+df_tabela = pd.read_csv('dados_ambientais.csv')
+colunas = 'classificacao_negros','classificacao_publica_rendamenor_autodeclara','classificacao_publica_rendamenor_autodeclara_pcd','classificacao_publica_rendamenor_naoautodeclara','classificacao_publica_rendamenor_naoautodeclara_pcd','classificacao_publica_rendamaior_autodeclara','classificacao_publica_rendamaior_autodeclara_pcd','classificacao_publica_rendamaior_naoautodeclara','classificacao_publica_rendamaior_naoautodeclara_pcd'
+
+df_tabela = df_tabela.replace('-', np.nan)
+
+for coluna in colunas:
+    df_tabela = df_tabela.loc[df_tabela[coluna].isna()]
+
+df_tabela = df_tabela[['inscricao','nome','nf_selecao','classificacao_su']].sort_values(by=['classificacao_su'])
+
+print(df_tabela.head(9))
