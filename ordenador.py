bf05c6338122669d185bbda7f76d198e5ce4e1eb
Juan Mangueira <juanmangueiraa@gmail.com>
2024-06-19 18:42:28 -0300

    Ajusta index

--- ordenador.py
+++ ordenador.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 
-df_tabela = pd.read_csv('dados_cursos_2023/dados_.csv')
+df_tabela = pd.read_csv('./dados_cursos_2024/dados_ciencias_amb.csv', skipinitialspace=True)
+print(df_tabela)
 colunas =   ('classificacao_negros',
             'classificacao_publica_rendamenor_autodeclara',
             'classificacao_publica_rendamenor_autodeclara_pcd',
@@ -12,6 +13,7 @@ colunas =   ('classificacao_negros',
             'classificacao_publica_rendamaior_naoautodeclara',
             'classificacao_publica_rendamaior_naoautodeclara_pcd')
 
+# df_tabela = df_tabela.replace('', '')
 df_tabela = df_tabela.replace('-', np.nan)
 
 for coluna in colunas:
@@ -23,6 +25,6 @@ df_tabela = df_tabela[[ 'inscricao',
                         'nf_selecao',
                         'classificacao_su']
                         ].sort_values(
-                        by=['classificacao_su'])
+                        by=['classificacao_su']).reset_index()
 
 print(df_tabela.head(50))
